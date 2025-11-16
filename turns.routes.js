6efb782801d631c66cf163fdaefcbaf4a7a5a365
marlyfr <marlypichol235@gmail.com>
2025-11-15 20:12:06 -0600

    turns.routes.js

--- turns.routes.js
+++ turns.routes.js
@@ -1,31 +1,22 @@
 import express from "express";
 import {
   createTurn,
-  getNextTurn,
-  callTurn,
-  startTurn,
-  finishTurn,
-  listTurns
-} from "../controllers/turns.controller.js";
+  listTurns,
+  getTurn,
+  updateTurn,
+  deleteTurn,
+  callNextTurn
+} from "./turns.controller.js";
 
 const router = express.Router();
 
-// Crear un turno
 router.post("/", createTurn);
-
-// Listar turnos (opcional ?areaId=1)
 router.get("/", listTurns);
+router.get("/:id", getTurn);
+router.put("/:id", updateTurn);
+router.delete("/:id", deleteTurn);
 
-// Obtener siguiente turno en espera por área
-router.get("/next/:areaId", getNextTurn);
-
-// Cambiar estado del turno → Llamando
-router.post("/call/:idTurno", callTurn);
-
-// Cambiar estado del turno → Atendiendo
-router.post("/start/:idTurno", startTurn);
-
-// Cambiar estado del turno → Finalizado
-router.post("/finish/:idTurno", finishTurn);
+// Especial
+router.post("/next", callNextTurn);
 
 export default router;
