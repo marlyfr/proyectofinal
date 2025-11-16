61eb9b0b0d4d974c94363cbaf73b9a13cac90606
marlyfr <marlypichol235@gmail.com>
2025-11-15 20:09:53 -0600

    Middleware

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,34 +1,19 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
 
 export default function authMiddleware(req, res, next) {
+  const header = req.headers.authorization;
+
+  if (!header)
+    return res.status(401).json({ error: "Falta header Authorization" });
+
+  const token = header.split(" ")[1];
+
   try {
-    const header = req.headers.authorization;
-
-    // No viene el header
-    if (!header) {
-      return res.status(401).json({ message: "Token requerido" });
-    }
-
-    // Formato: "Bearer TOKEN"
-    const parts = header.split(" ");
-    if (parts.length !== 2 || parts[0] !== "Bearer") {
-      return res.status(401).json({ message: "Formato de token inválido" });
-    }
-
-    const token = parts[1];
-
-    // Verificar token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Guardar datos del usuario en req.user
-    req.user = decoded; // { id, email, rol }
-
+    req.user = decoded;
     next();
-
   } catch (err) {
-    console.error("authMiddleware error:", err);
-    return res.status(401).json({ message: "Token inválido o expirado" });
+    res.status(403).json({ error: "Token inválido" });
   }
 }
+
